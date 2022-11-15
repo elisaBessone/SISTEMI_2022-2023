6805aa7a8fd139b3a5335c7db7d536fbeaf4ec99
Elisa Bessone <elisa.bessone@itiscuneo.eu>
2022-11-15 17:39:28 +0100

    Esercizio crittografia Diffie-Hellman

--- Python/001_chiave/common.py
+++ Python/001_chiave/common.py
@@ -1,30 +1,15 @@
 import random
 import config
 
-"""def modexp(a,b,n):
-    print(f"a {a}, b {b}, n {n}")
+def modexp(a,b,n):
     acc = 1
     while b > 0:
         if b % 2 == 1:
             acc = (acc*a) % n
-            print(f"1ACC: {acc}")
         a = (a * a) % n
-        print(f"a {a}")
         b = b // 2
-        print(f"b {b}")
-        print(f"ACC: {acc}")
         
-    print("fine ciclo")
-    return acc"""
-
-def modexp(b, exp, m):
-    res = 1
-    while exp > 1:
-        if exp & 1:
-            res = (res * b) % m
-        b = b ** 2 % m
-        exp >>= 1
-    return (b * res) % m
+    return acc
 
 #number -> string -> bytes
 def encode_big(num):
